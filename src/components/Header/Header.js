c66a94d9e64ce0135b2c5fbbf6400660bf901fa9
anikjoyy <anikjoy.cse@gmail.com>
2022-03-26 04:23:59 +0600

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header-design'>
+      <h1>Welcome to the Bonsai World</h1>
+    </div>
+  );
+};
+
+export default Header;
